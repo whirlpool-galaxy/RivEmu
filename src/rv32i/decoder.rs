b3fe296cd91ff86edf017afc378eb95ce074815d
Jonathan Schild <js.jonathan.schild@gmail.com>
2022-05-02 21:28:54 +0200

    Add func decoder implementation

--- src/rv32i/decoder.rs
+++ src/rv32i/decoder.rs
@@ -2,6 +2,7 @@
  * Copyright (C) 2022 Jonathan Schild - All Rights Reserved
  */
 
+use super::executor::*;
 use super::BaseInstruction;
 use super::RV32IInterface;
 use crate::utility::*;
@@ -51,14 +52,6 @@ const OPCODE_DECODING_TABLE: [[Operation; 8]; 4] = [
         Operation::Bit80,
     ],
 ];
-
-trait RV32IExecutor {
-    fn get_executor(
-        opcode: u8,
-        func: u16,
-        instruction: BaseInstruction,
-    ) -> fn(cpu: &mut dyn RV32IInterface, instruction: BaseInstruction);
-}
 
 enum Operation {
     Compressed,
@@ -210,6 +203,144 @@ impl Operation {
             Operation::Bit80 => {}
         };
         (func, instr)
+    }
+
+    fn get_executor(
+        &self,
+        func: Option<u16>,
+    ) -> fn(cpu: &mut dyn RV32IInterface, instruction: BaseInstruction) {
+        let mut ret = unknown as fn(cpu: &mut dyn RV32IInterface, instruction: BaseInstruction);
+        match self {
+            Operation::Compressed => {}
+            Operation::Load => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b000 => ret = lb,
+                    0b001 => ret = lh,
+                    0b010 => ret = lw,
+                    0b100 => ret = lbu,
+                    0b101 => ret = lhu,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::LoadFp => {}
+            Operation::Custom0 => {}
+            Operation::MiscMem => {
+                // Unknown
+            }
+            Operation::OpImm => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b000 => ret = addi,
+                    0b010 => ret = slti,
+                    0b011 => ret = sltiu,
+                    0b100 => ret = xori,
+                    0b110 => ret = ori,
+                    0b111 => ret = andi,
+                    0b0000000001 => ret = slli,
+                    0b0000000101 => ret = srli,
+                    0b0100000101 => ret = srai,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::Auipc => match func {
+                Some(_) => panic!("Invalid function code!"),
+                None => ret = auipc,
+            },
+            Operation::OpImm32 => {}
+            Operation::Bit48_0 => {}
+            Operation::Store => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b000 => ret = sb,
+                    0b001 => ret = sh,
+                    0b010 => ret = sw,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::StoreFp => {}
+            Operation::Custom1 => {}
+            Operation::Amo => {}
+            Operation::Op => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b0000000000 => ret = add,
+                    0b0100000000 => ret = sub,
+                    0b0000000001 => ret = sll,
+                    0b0000000010 => ret = slt,
+                    0b0000000011 => ret = sltu,
+                    0b0000000100 => ret = xor,
+                    0b0000000101 => ret = srl,
+                    0b0100000101 => ret = sra,
+                    0b0000000110 => ret = or,
+                    0b0000000111 => ret = and,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::Lui => match func {
+                Some(_) => panic!("Invalid function code!"),
+                None => ret = lui,
+            },
+            Operation::Op32 => {}
+            Operation::Bit64 => {}
+            Operation::Madd => {}
+            Operation::Msub => {}
+            Operation::Nmsub => {}
+            Operation::Nmadd => {}
+            Operation::OpFp => {}
+            Operation::Reserved0 => {}
+            Operation::Custom2 => {}
+            Operation::Bit48_1 => {}
+            Operation::Branch => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b000 => ret = beq,
+                    0b001 => ret = bne,
+                    0b100 => ret = blt,
+                    0b101 => ret = bge,
+                    0b110 => ret = bltu,
+                    0b111 => ret = bgeu,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::Jalr => {
+                let func = match func {
+                    Some(x) => x,
+                    None => panic!("Invalid function code!"),
+                };
+                match func {
+                    0b000 => ret = jalr,
+                    _ => panic!("Invalid function code!"),
+                };
+            }
+            Operation::Reserved1 => {}
+            Operation::Jal => match func {
+                Some(_) => panic!("Invalid function code!"),
+                None => ret = jal,
+            },
+            Operation::System => {
+                // Unknown
+            }
+            Operation::Reserved2 => {}
+            Operation::Custom3 => {}
+            Operation::Bit80 => {}
+        };
+        ret
     }
 }
 
