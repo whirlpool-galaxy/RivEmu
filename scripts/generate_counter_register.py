13c37a03d4e2057b1e5a81b283f177d265d69996
Jonathan "Nath" Schild <nath@j-schild.de>
2022-09-18 20:27:07 +0200

    Add csv with Mcsr

---
+++ scripts/generate_counter_register.py
@@ -0,0 +1,18 @@
+start = 0x3A0
+for i in range(0, 15):
+    print("0x{:3X};MRW;pmpcfg{}".format(start + i, i))
+print("=============================")
+
+start = 0x3B0
+for i in range(0, 63):
+    print("0x{:3X};MRW;pmpaddr{}".format(start + i, i))
+print("=============================")
+
+start = 0xB03
+for i in range(3, 31):
+    print("0x{:3X};MRW;mhpmcounter{}".format(start + i -3, i))
+print("=============================")
+
+start = 0x323
+for i in range(3, 31):
+    print("0x{:3X};MRW;mhpmevent{}".format(start + i -3, i))
--- src/base_isa/zicsr/mod.rs
+++ src/base_isa/zicsr/mod.rs
@@ -10,3 +10,29 @@
 //! Copyright (C) 2022 Jonathan Schild - MIT License
 //!  
 //! - Jonathan Schild
+
+enum ZiscrVal {
+    Mxlen64(u64),
+    Mxlen32(u32),
+}
+
+enum AccessMode {
+    DRW,
+    MRW,
+    MRO,
+    HRW,
+    HRO,
+    SRW,
+    SRO,
+    URW,
+    URO,
+}
+
+trait ZicsrRegister {
+    fn get_access_mode() -> AccessMode;
+    fn get_zicsr() -> ZiscrVal;
+    fn set_zicsr(value: ZiscrVal) -> bool;
+}
+
+mod machine {
+}
